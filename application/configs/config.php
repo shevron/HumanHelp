96bffae6e32c9c944f31c0f2e3abdb0c205fb4aa
Shahar Evron <shahar.evron@gmail.com>
2010-01-07 14:03:33 +0200

    Removing reCaptcha keys from config file

--- application/configs/config.php
+++ application/configs/config.php
@@ -5,13 +5,17 @@
  */
 
 return array(
+    /* The default book to view if no book is specified in the URL */
     'defaultBook' => 'zend-server',
 
+    /* Uncomment the next lines to enable reCaptcha protection for comments */
+    /*
     'captcha' => array(
         'type' => 'recaptcha',
         'options' => array(
-            'publickey'  => '6LfEWgoAAAAAAI0WlkYqb0MbfFKz9X_V8B1CVRpY',
-            'privatekey' => '6LfEWgoAAAAAAFfsHs_0VK85Lxi0UZlsjRoOM2dJ'
+            'publickey'  => 'yourpublickey',
+            'privatekey' => 'yourprivatekey'
         )
     )
+    */
 );
